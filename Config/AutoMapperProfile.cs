dcb5cea23426f6d8df5b706c865e7952e83c91c5
LilAguh <aguezeochoa@gmail.com>
2026-04-22 18:54:28 -0300

    chore: agregar AutoMapperProfile con mapeos de entidades y DTOs

---
+++ Config/AutoMapperProfile.cs
@@ -0,0 +1,42 @@
+using AutoMapper;
+using Models;
+using Services.DTOs.Requests;
+using Services.DTOs.Responses;
+
+namespace Config;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        // Client mappings
+        CreateMap<Client, ClientDto>();
+        CreateMap<CreateClientRequest, Client>();
+        CreateMap<UpdateClientRequest, Client>();
+
+        // User mappings
+        CreateMap<User, UserDto>();
+
+        // Ticket mappings
+        CreateMap<Ticket, TicketDto>();
+        CreateMap<Ticket, TicketDetailDto>();
+        CreateMap<CreateTicketRequest, Ticket>()
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => Models.Enums.TicketState.Nuevo))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.LastUpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
+
+        // TicketHistory mappings
+        CreateMap<TicketHistory, TicketHistoryDto>();
+
+        // Account mappings
+        CreateMap<Account, AccountDto>();
+        CreateMap<CreateAccountRequest, Account>()
+            .ForMember(dest => dest.CurrentBalance, opt => opt.MapFrom(src => src.InitialBalance));
+
+        // BoxMovement mappings
+        CreateMap<BoxMovement, BoxMovementDto>()
+            .ForMember(dest => dest.TicketNumber, opt => opt.MapFrom(src => src.Ticket != null ? src.Ticket.Number : (int?)null));
+        CreateMap<RegisterMovementRequest, BoxMovement>()
+            .ForMember(dest => dest.RegisteredAt, opt => opt.MapFrom(src => DateTime.UtcNow));
+    }
+}
