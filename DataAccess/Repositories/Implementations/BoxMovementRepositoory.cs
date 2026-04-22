76c84734bfa1cc002a1ae3daca987350fbe2cedd
LilAguh <aguezeochoa@gmail.com>
2026-04-22 13:50:59 -0300

    chore: implementacion del repos itorio del box movement

---
+++ DataAccess/Repositories/Implementations/BoxMovementRepositoory.cs
@@ -0,0 +1,57 @@
+using DataAccess.Context;
+using DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Models.Enums;
+
+namespace DataAccess.Repositories.Implementations;
+
+public class BoxMovementRepository : GenericRepository<BoxMovement>, IBoxMovementRepository
+{
+    public BoxMovementRepository(ProxarDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<BoxMovement>> GetByAccountAsync(Guid accountId)
+    {
+        return await _dbSet
+            .Include(m => m.Account)
+            .Include(m => m.Ticket)
+            .Include(m => m.User)
+            .Where(m => m.AccountId == accountId)
+            .OrderByDescending(m => m.MovementDate)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<BoxMovement>> GetByTicketAsync(Guid ticketId)
+    {
+        return await _dbSet
+            .Include(m => m.Account)
+            .Include(m => m.User)
+            .Where(m => m.TicketId == ticketId)
+            .OrderByDescending(m => m.MovementDate)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<BoxMovement>> GetByDateRangeAsync(DateTime startDate, DateTime endDate)
+    {
+        return await _dbSet
+            .Include(m => m.Account)
+            .Include(m => m.Ticket)
+            .Include(m => m.User)
+            .Where(m => m.MovementDate >= startDate && m.MovementDate <= endDate)
+            .OrderByDescending(m => m.MovementDate)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<BoxMovement>> GetByTypeAsync(MovementType type)
+    {
+        return await _dbSet
+            .Include(m => m.Account)
+            .Include(m => m.Ticket)
+            .Include(m => m.User)
+            .Where(m => m.Type == type)
+            .OrderByDescending(m => m.MovementDate)
+            .ToListAsync();
+    }
+}
