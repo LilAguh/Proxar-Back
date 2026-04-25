169e264d474b19871e216cbd716784fb64dcb6e2
LilAguh <aguezeochoa@gmail.com>
2026-04-25 14:56:33 -0300

    Update CreateCompanyRequest.cs

---
+++ Services/DTOs/Requests/CreateCompanyRequest.cs
@@ -0,0 +1,8 @@
+namespace Services.DTOs.Requests;
+
+public class CreateCompanyRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public string? LogoUrl { get; set; }
+}
