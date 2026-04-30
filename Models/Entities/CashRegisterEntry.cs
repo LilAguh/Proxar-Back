230a7f76ca545b296a5629c6e7085f5a0caf0427
LilAguh <aguezeochoa@gmail.com>
2026-04-30 19:35:18 -0300

    backend: agrega detalle de caja

---
+++ Models/Entities/CashRegisterEntry.cs
@@ -0,0 +1,15 @@
+namespace Models;
+
+public class CashRegisterEntry
+{
+    public Guid Id { get; set; }
+
+    public Guid CashRegisterId { get; set; }
+    public CashRegister CashRegister { get; set; } = null!;
+
+    public Guid AccountId { get; set; }
+    public Account Account { get; set; } = null!;
+
+    public decimal OpeningAmount { get; set; }
+    public decimal? ClosingAmount { get; set; }
+}
