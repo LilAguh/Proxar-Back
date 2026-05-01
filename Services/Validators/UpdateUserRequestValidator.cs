691b7be63db84403d48b783627d7a4b18dbc85c2
LilAguh <aguezeochoa@gmail.com>
2026-04-30 21:33:09 -0300

    agrega validacion de actualizacion de usuario

---
+++ Services/Validators/UpdateUserRequestValidator.cs
@@ -0,0 +1,22 @@
+using FluentValidation;
+using Services.DTOs.Requests;
+
+namespace Services.Validators;
+
+public class UpdateUserRequestValidator : AbstractValidator<UpdateUserRequest>
+{
+    public UpdateUserRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("El nombre es requerido")
+            .MaximumLength(100).WithMessage("El nombre no puede exceder 100 caracteres");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("El email es requerido")
+            .EmailAddress().WithMessage("El email no es válido")
+            .MaximumLength(100).WithMessage("El email no puede exceder 100 caracteres");
+
+        RuleFor(x => x.Role)
+            .IsInEnum().WithMessage("El rol no es válido");
+    }
+}
