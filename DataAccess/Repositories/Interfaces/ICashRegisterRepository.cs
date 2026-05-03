ea9a9d4f0a430873371961ee715602a735254c1f
LilAguh <aguezeochoa@gmail.com>
2026-05-02 22:03:46 -0300

    feat: extender contrato de caja abierta

--- DataAccess/Repositories/Interfaces/ICashRegisterRepository.cs
+++ DataAccess/Repositories/Interfaces/ICashRegisterRepository.cs
@@ -6,8 +6,8 @@ public interface ICashRegisterRepository
 {
     Task<CashRegister?> GetByIdAsync(Guid id, Guid companyId);
     Task<CashRegister?> GetOpenAsync(Guid companyId);
-    Task<CashRegister?> GetTodayAsync(Guid companyId, DateTime businessDate);
-    Task<CashRegister?> GetPreviousClosedAsync(Guid companyId, DateTime beforeDate);
+    Task<CashRegister?> GetTodayAsync(Guid companyId, DateOnly businessDate);
+    Task<CashRegister?> GetPreviousClosedAsync(Guid companyId, DateOnly beforeDate);
     Task<IEnumerable<CashRegister>> GetHistoryAsync(Guid companyId, int page, int pageSize);
     Task<CashRegister> AddAsync(CashRegister register);
     Task UpdateAsync(CashRegister register);
